e2addfb708b6b847e0e2f147fc16ca3ed649d2de
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-10-05 16:01:41 +0700

    Add files via upload

---
+++ strlen/strlen_function.php
@@ -0,0 +1,13 @@
+<?php
+$str = 'abcdef';
+echo strlen($str)."<br>";//6
+
+$str = ' ab cd ';
+echo strlen($str)."<br>";//7
+
+$str = 92780;
+echo strlen($str)."<br>";//5
+
+$str = '';
+echo strlen($str)."<br>";//0
+?>
