9206004235b13d3b4e34d6a416dd299ef16f7d2b
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-10-07 18:26:29 +0700

    Add files via upload

---
+++ str_split/str_split_function.php
@@ -0,0 +1,12 @@
+<?php
+$str = "Hello PHP programming language";
+$arr1 = str_split($str);
+$arr2 = str_split($str, 1);
+$arr3 = str_split($str, 3);
+print_r($arr1);
+print_r($arr2);
+print_r($arr3);
+$str = 2578978;
+$arr1 = str_split($str);
+print_r($arr1);
+?>
