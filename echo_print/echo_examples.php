c0f4c6d34633ef67be3dd9e8a6316e3e2acf49c8
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-10-10 17:07:12 +0700

    Add files via upload

---
+++ echo_print/echo_examples.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello PHP."."<br>";//Hello PHP.
+echo ("Hello PHP.")."<br>";////Hello PHP.
+echo "Hello", "PHP"."<br>";//HelloPHP
+echo ("Hello"), ("PHP")."<br>";//HelloPHP
+echo "Hello PHP
+PHP is so great
+I love it"."<br>";//Hello PHP PHP is so great I love it
+echo "Hello PHP\nPHP is so great\nI love it"."<br>";//Hello PHP PHP is so great I love it
+$value = 5;
+echo "I have $value apples"."<br>";//I have 5 apples
+echo "I have ".$value. " apples"."<br>";//I have 5 apples
+echo 6 * 7;//42
+echo "<br>";
+$some_var = true;
+echo $some_var ? 'true'."<br>": 'false'."<br>";//true
+?>
+
---
+++ echo_print/print_examples.php
@@ -0,0 +1,16 @@
+<?php
+print "Hello PHP."."<br>";//Hello PHP.
+print ("Hello PHP.")."<br>";////Hello PHP.
+print "Hello PHP
+PHP is so great
+I love it"."<br>";//Hello PHP PHP is so great I love it
+print "Hello PHP\nPHP is so great\nI love it"."<br>";//Hello PHP PHP is so great I love it
+$value = 5;
+print "I have $value apples"."<br>";//I have 5 apples
+print "I have ".$value. " apples"."<br>";//I have 5 apples
+print 6 * 7;//42
+print "<br>";
+$some_var = true;
+print $some_var ? 'true'."<br>": 'false'."<br>";//true
+?>
+
